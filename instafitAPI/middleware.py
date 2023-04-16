a3125c53c9d848c8fe7ed12ce9693f62ddfe42e8
killuh <20297127+killuhwhale@users.noreply.github.com>
2023-04-15 23:16:59 -0700

    middleware fix

--- instafitAPI/middleware.py
+++ instafitAPI/middleware.py
@@ -3,17 +3,26 @@ from jwt import decode, ExpiredSignatureError
 from django.contrib.auth import get_user_model
 from instafitAPI.settings  import JWT_KEY
 User =  get_user_model()
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class JWTMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        print(f"middleware req: {request.method=}, {request.path=}")
+        logger.critical(f"middleware req: {request.method=}, {request.path=} ")
         # Allow requests to endpoints without auth token
-        if request.path in ['/login/', '/register/', '/token/', '/token/refresh/', '/users/', '/emailvalidation/confirm_email/', '/emailvalidation/send_confirmation_email']:
-            response = self.get_response(request)
-            return response
+        if request.path in ['/login/', '/register/', '/token/', '/token/refresh/', '/users/', '/user/send_reset_code/', '/emailvalidation/confirm_email/', '/emailvalidation/send_confirmation_email']:
+            if request.path == "/users/" and not request.method == "post":
+                print("Needs access token... go along and procedd")
+                # return JsonResponse({'error': f'Invalid access token, route not permitted '}, status=401)
+            else:
+                response = self.get_response(request)
+                return response
+
 
         authorization = request.META.get('HTTP_AUTHORIZATION')
         if not authorization:

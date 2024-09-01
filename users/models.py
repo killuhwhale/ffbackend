18bf02744ebbafa465ea0095191254e7f0e5b2d3
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-08-31 20:58:48 -0700

    logging to register user

--- users/models.py
+++ users/models.py
@@ -115,6 +115,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
+
+
+
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)

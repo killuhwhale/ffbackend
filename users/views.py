445eb22ad941c62107bd52d723b2cdcfba61cbfe
killuh <20297127+killuhwhale@users.noreply.github.com>
2025-04-28 21:05:57 -0700

    import status

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import Group
 from django.db.utils import IntegrityError
 import pytz
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
